c8e56c17c805deebbd6056bf79f40adb5a9dd79a
djurny <tom.urlings@gmail.com>
2024-09-27 16:48:52 -0700

    Update docker-entrypoint.sh
    Added a check to make sure database.db is still a sane file to process. If the file appears to be corrupt (e.g. not JSON) due to some issue with writing it out, or a crash of z2m, it will move the corrupt database.db out of the way and create an empty database.db to allow z2m to startup.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -14,4 +14,27 @@ if [ ! -f "$DATA/configuration.yaml" ]; then
     cp /app/configuration.example.yaml "$DATA/configuration.yaml"
 fi
 
+DATABASE="${DATA:?}/database.db"
+case `file -b -i "${DATABASE:?}"` in
+    application/*json|application/*jason)
+        echo "Database seems sane..."
+        ;;
+    *)
+        DATABASE_BACKUP="${DATABASE:?}.`date -Isec`~"
+        if mv "${DATABASE:?}" "${DATABASE_BACKUP:?}"
+        then
+            echo "WARNING: Corrupt database has renamed to ${DATABASE_BACKUP:-N/A}..." 1>&2
+        else
+            echo "WARNING: Removing corrupt database..." 1>&2
+            rm "${DATABASE:?}"
+        fi
+        if : > "${DATABASE:?}"
+        then
+            echo "Starting with empty database..." 
+        else
+            echo "WARNING: Could not create empty database!" 1>&2
+        fi
+    ;;
+esac
+
 exec "$@"
